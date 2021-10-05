63157453bdc44c2fa58db396004c8d629ddb3e5e
Gourab Banik <gourabbanik2399@gmail.com>
2021-10-06 02:37:21 +0530

    24. Changing Navigation Link to Login

--- instagram/src/components/reducer/userReducer.js
+++ instagram/src/components/reducer/userReducer.js
@@ -3,6 +3,7 @@ export const initialState =null
 
 export const reducer=(state,action)=>{
     if(action.type ==="USER"){
+        //console.log("action.payload",action.payload)
         return action.payload
     }
     return action.payload
